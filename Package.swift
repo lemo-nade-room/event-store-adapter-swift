9b89655bef7c6a5575ecff1588da78ba555dc454
tanaka <lemo.nade.engineer.room@gmail.com>
2024-12-22 15:05:44 +0900

    Targetを定義

--- Package.swift
+++ Package.swift
@@ -5,20 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "event-store-adaptor",
+    platforms: [.macOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "event-store-adaptor",
-            targets: ["event-store-adaptor"])
+            name: "EventStoreAdaptor",
+            targets: ["EventStoreAdaptor"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "event-store-adaptor"),
+            name: "EventStoreAdaptor"),
         .testTarget(
-            name: "event-store-adaptorTests",
-            dependencies: ["event-store-adaptor"]
+            name: "EventStoreAdaptorTests",
+            dependencies: ["EventStoreAdaptor"]
         ),
     ]
 )
--- Tests/EventStoreAdaptorTests/EventStoreAdaptorTests.swift
+++ Tests/EventStoreAdaptorTests/EventStoreAdaptorTests.swift
@@ -1,6 +1,6 @@
 import Testing
 
-@testable import event_store_adaptor
+@testable import EventStoreAdaptor
 
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
