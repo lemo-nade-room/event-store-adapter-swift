6e3790d7c2d83abe0eacd4033ebee741d5946db9
tanaka <lemo.nade.engineer.room@gmail.com>
2025-01-27 22:51:50 +0900

    fix: 🐛 テスト設定でDynamoDBエンドポイントをlocalhostに修正

--- Tests/EventStoreAdaptorTests/EventStoreForDynamoDBTests.swift
+++ Tests/EventStoreAdaptorTests/EventStoreForDynamoDBTests.swift
@@ -164,7 +164,7 @@ where
             config: .init(
                 ignoreConfiguredEndpointURLs: true,
                 region: "ap-northeast-1",
-                endpoint: "http://dynamodb:8000",
+                endpoint: "http://localhost:8000",
                 httpClientEngine: AsyncHTTPClientEngine(httpClient: .shared)
             ))
 
