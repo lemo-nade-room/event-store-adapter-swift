e2e487b61926aa738ea6415f2437af9555240a67
tanaka <lemo.nade.engineer.room@gmail.com>
2025-03-22 02:25:19 +0900

    docs: 📝 AggregateIdプロトコルのコメントを拡充

--- Sources/EventStoreAdapter/AggregateId.swift
+++ Sources/EventStoreAdapter/AggregateId.swift
@@ -1,7 +1,19 @@
-/// 集約IDを表すためのProtocol
+/// Represents a protocol for defining an Aggregate ID, ensuring the type is `Sendable`, `Hashable`, `Codable`,
+/// and `LosslessStringConvertible`.
+///
+/// In most CQRS/Event Sourcing contexts, an `AggregateId` uniquely identifies a specific category or type of aggregate.
+///
+/// # Japanese
+/// 集約IDを定義するためのプロトコルです。
+/// このプロトコルは、`Sendable`, `Hashable`, `Codable`, `LosslessStringConvertible` をすべて満たす必要があります。
+/// CQRS/Event Sourcing において、`AggregateId` は特定の集約（エンティティ）の種類や識別を一意に行うために使用されます。
 public protocol AggregateId: Sendable, Hashable, Codable, LosslessStringConvertible {
-    /// 集約の種別名を返す
+    /// Returns the name of this aggregate type.
     ///
-    /// 集約の型ごとに一意である必要がある
+    /// This name must be unique for each aggregate type within the system.
+    ///
+    /// # Japanese
+    /// 集約の種別名を返します。
+    /// システム内で集約の型ごとに一意となる必要があります。
     static var name: String { get }
 }
