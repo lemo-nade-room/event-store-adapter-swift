2173cc6f61bfdc349650b9f90e40064fa4d8061e
Yoichiro Tanaka <lemo.nade.engineer.room@gmail.com>
2026-09-19 07:45:24 +0900

    fix: 🐛 #97 swift-cryptoの許容範囲を3系から5系に拡張

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-configuration", from: "1.0.0"),
-    .package(url: "https://github.com/apple/swift-crypto.git", from: "3.0.0"),
+    .package(url: "https://github.com/apple/swift-crypto.git", "3.0.0" ..< "6.0.0"),
     .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
     .package(url: "https://github.com/soto-project/soto.git", from: "7.0.0"),
     .package(url: "https://github.com/soto-project/soto-core.git", from: "7.0.0"),
